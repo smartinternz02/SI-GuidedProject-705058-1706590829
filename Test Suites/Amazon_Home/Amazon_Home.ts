7aa1f7417b3ca217c0e490e37162657bcacff694
Deepika Cheruku <157818801+DeepikaCheruku@users.noreply.github.com>
2024-01-30 10:59:59 +0530

    Add files via upload

---
+++ Test Suites/Amazon_Home/Amazon_Home.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Amazon_Home</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>21f30d78-d3b0-4192-82d5-3a37bdb343b5</testSuiteGuid>
+   <testCaseLink>
+      <guid>a2eecba2-3083-44fb-8db8-f21cddf35419</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_Registration_001</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4083dd6c-27ab-4c02-ac37-198301d7c919</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_SignIn_002</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b597e4ee-65b6-422b-87fe-b394a734dbac</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_SV_003</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f9cce8e8-58a6-4140-b379-c8ff985d2e77</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/DDT_TestData/Amazon_Search_excel_folder</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f9cce8e8-58a6-4140-b379-c8ff985d2e77</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>category</value>
+         <variableId>8da9dc91-9fb4-4f81-9519-8e886bcec16a</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f9cce8e8-58a6-4140-b379-c8ff985d2e77</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>item</value>
+         <variableId>99393b23-cf64-487b-8f26-df8f45d08007</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b6025676-dc3e-4a07-9322-fae517f28b2b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_TodaysDeals_004</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>952035b0-1729-4854-b4ec-f8a79c2effcd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_AddToCart_005</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5581f5a9-4e31-4f72-83e5-7d73d6173fbf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_Order_006</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3ccc4877-f469-46d9-938f-92d977656501</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_FooterLinks_007</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0ebb30ca-7328-4821-a421-8b88c7ca6528</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_Language_008</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c5a3fbc3-f9e2-46c9-b922-f153506fd277</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/AmazonProject/TC_Amazon_Home_SignOut_009</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
